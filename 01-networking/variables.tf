be3232cd8d64bca30a13e148a8d82e72a8d4da58
hmaiajunior <juniorce04@gmail.com>
2025-08-27 08:53:13 -0300

    eks-express-01-networking-configuration-iac

---
+++ 01-networking/variables.tf
@@ -0,0 +1,117 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "assume_role" {
+  type = object({
+    role_arn    = string
+    external_id = string
+
+  })
+
+  default = {
+    role_arn    = "arn:aws:iam::353203138858:role/devopsnanuvem-week"
+    external_id = "97b9dcb3-26e7-4798-830d-79f3dd893374"
+
+  }
+  
+}
+
+variable "tags" {
+  type = object({
+    Project     = string
+    Environment = string
+  })
+
+  default = {
+    Project     = "eks-express",
+    Environment = "production"
+  }
+}
+
+variable "aws_s3_bucket_versioning" {
+  type = string
+  default = "Enabled"
+  
+}
+
+variable "vpc" {
+  type = object({
+    name                     = string
+    cidr_block               = string
+    internet_gateway_name    = string
+    nat_gateway_name         = string
+    public_route_table_name  = string
+    private_route_table_name = string
+    eip_name                 = string
+    eks_cluster_name_tag     = string
+    public_subnets = list(object({
+      name                    = string
+      cidr_block              = string
+      availability_zone       = string
+      map_public_ip_on_launch = bool
+    }))
+    private_subnets = list(object({
+      name                    = string
+      cidr_block              = string
+      availability_zone       = string
+      map_public_ip_on_launch = bool
+    }))
+    observability_subnets = list(object({
+      name                    = string
+      cidr_block              = string
+      availability_zone       = string
+      map_public_ip_on_launch = bool
+    }))
+  })
+
+  default = {
+    name                     = "eks-express-vpc"
+    cidr_block               = "10.0.0.0/24"
+    internet_gateway_name    = "internet-gateway"
+    public_route_table_name  = "public-route-table"
+    private_route_table_name = "private-route-table"
+    nat_gateway_name         = "nat-gateway"
+    eip_name                 = "nat-gateway-eip"
+    eks_cluster_name_tag     = "eks-express-cluster"
+    public_subnets = [{
+      name                    = "public-subnet-us-east-1a"
+      cidr_block              = "10.0.0.0/27"
+      availability_zone       = "us-east-1a"
+      map_public_ip_on_launch = true
+      },
+      {
+        name                    = "public-subnet-us-east-1b"
+        cidr_block              = "10.0.0.64/27"
+        availability_zone       = "us-east-1b"
+        map_public_ip_on_launch = true
+    }]
+    private_subnets = [{
+      name                    = "private-subnet-us-east-1a"
+      cidr_block              = "10.0.0.32/27"
+      availability_zone       = "us-east-1a"
+      map_public_ip_on_launch = false
+      },
+      {
+        name                    = "private-subnet-us-east-1b"
+        cidr_block              = "10.0.0.96/27"
+        availability_zone       = "us-east-1b"
+        map_public_ip_on_launch = false
+    }]
+    observability_subnets = [{
+      name                    = "private-observability-subnet-us-east-1a"
+      cidr_block              = "10.0.0.128/27"
+      availability_zone       = "us-east-1a"
+      map_public_ip_on_launch = false
+      },
+      {
+        name                    = "private-observability-subnet-us-east-1b"
+        cidr_block              = "10.0.0.160/27"
+        availability_zone       = "us-east-1b"
+        map_public_ip_on_launch = false
+    }]
+  }
+}
+
+
