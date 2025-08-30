27dd6c5f5f504592ae1255be37245290b196c530
hmaiajunior <juniorce04@gmail.com>
2025-08-29 23:32:27 -0300

    eks-express-02-eks-cluster-deploy

---
+++ 02-eks-cluster/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "eks-express-terraform-state-files-01"
+    key            = "cluster/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "eks-express-terraform-state-locking-01"
+  }
+}
+
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = var.tags
+  }
+  
+  assume_role {
+    role_arn    = var.assume_role.role_arn
+    #external_id = var.assume_role.external_id
+  }
+}
+
+
+provider "helm" {
+  kubernetes = {
+    host                   = aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.id]
+      command     = "aws"
+    }
+  }
+}
--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Repositório do curso:
 
 https://github.com/kenerry-serain/eks-express-iac
+
+
+https://github.com/kubernetes-sigs/metrics-server
+
+https://github.com/kubernetes-sigs/aws-ebs-csi-driver
+
+https://github.com/kubernetes-sigs/external-dns
